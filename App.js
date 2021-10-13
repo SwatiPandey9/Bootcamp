911e81db557c924b37bfae20b2a580cc35f45449
Swati Pandey <swati@TTNs-MacBook-Pro.local>
2021-10-14 05:04:33 +0530

    commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import ApiList from './src/screen/ApiList';
+
+ const App = () => {
+   return(
+     <View style = {styles.container}>
+       <ApiList />
+     </View>
+   )
+ }
+
+ const styles = StyleSheet.create({
+   container:{
+     flex:1,
+     
+   }
+ })
+
+export default App;
---
+++ src/screen/ApiList.js
@@ -0,0 +1,105 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Image,
+} from 'react-native';
+
+const ApiList = () => {
+  const listItem = ({item}) => {
+    return (
+      <View style={styles.listContainer}>
+        <View>
+          <Image style={styles.itemImage} source={{uri: `${item.avatar}`}} />
+        </View>
+        <View>
+          <Text style={styles.itemText}>
+            First_name:  {item.first_name} {'\n'}
+            Last_name:  {item.last_name}{'\n'}
+            Email:  {item.email}        
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  const getData = (pageNo = 1, refreshing = true) => {
+    setRefresh(refreshing);
+    fetch(`https://reqres.in/api/users?page=${pageNo}`, {method: 'GET'})
+      .then((response) => response.json())
+      .then((json) => {
+        setTimeout(() => {
+          setRefresh(false);
+        }, 1000);
+
+        setData({
+          pageNo,
+          listData: pageNo === 1 ? json.data : [...data.listData, ...json.data],
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const [refreshing, setRefresh] = useState(false);
+
+  const [data, setData] = useState({pageNo: 1, listData: []});
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <FlatList
+        data={data.listData}
+        renderItem={listItem}
+        keyExtractor={(item, index) => index.toString()}
+        refreshing={refreshing}
+        onRefresh={() => getData(1, true)}
+        onEndReached={() => {
+          getData(data.pageNo + 1);
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  listContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    padding: 70,
+    paddingHorizontal: 2,
+    paddingTop: 20,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    flexDirection: 'row',
+    backgroundColor:'#f2f2f2'
+  },
+  itemImage: {
+    marginTop:30,
+    height: 100,
+    width: 100,
+    borderWidth: 1,
+    marginRight:5,
+    borderRadius:50
+  },
+  itemText: {
+
+    marginTop: 40,
+    paddingLeft: 10,
+    fontSize: 17,
+    fontWeight:'bold'
+  },
+});
+
+export default ApiList;
