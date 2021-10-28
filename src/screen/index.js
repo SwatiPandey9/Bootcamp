f5246b1656846a067789fbd4799f57e50cdc3a37
SwatiPandey9 <91865197+SwatiPandey9@users.noreply.github.com>
2021-10-29 03:50:54 +0530

    Delete index.js

--- src/screen/index.js
+++
@@ -1,8 +0,0 @@
-import Mapview from './Mapview'
-import Animations from './Animations';
-import Gesture from './Gesture';
-import Contents from './Contents'
-import HomeScreen from './HomeScreen'
-import Profile from './Profile'
-
-export {Mapview, Animations, Gesture, Contents, HomeScreen, Profile} ;
