8bc93efb9d20fd5e4067ff085068c529249f6909
SwatiPandey9 <91865197+SwatiPandey9@users.noreply.github.com>
2021-10-30 08:28:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -16,33 +16,4 @@ const App = () => {
     );
 };
 
-// const App = () => {
-//   return (
-//     <Provider store={store} >  
-//       <Counter />
-//     </Provider>
-
-//     );
-// };
-
-// import React from 'react';
-
-// import {NavigationContainer} from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-// import {HomeScreen, Profile} from './src/screen'
-// import { Provider } from 'react-redux';
-// import store from './src/redux/store';
-// import Counter from './src/screen/Counter';
-
-// const Tab = createBottomTabNavigator();
-
-// const App = () => {
-//   return(
-//     <Provider store={store}>
-//       <Counter />
-//     </Provider>
-//   )
-// };
-
 export default App;
