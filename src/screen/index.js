452886272c48caf7685e5e0b2977c563cdd74685
SwatiPandey9 <91865197+SwatiPandey9@users.noreply.github.com>
2021-11-03 14:35:21 +0530

    Update index.js

--- src/screen/index.js
+++ src/screen/index.js
@@ -1,9 +1,4 @@
-import Mapview from './Mapview'
-import Animations from './Animations';
-import Gesture from './Gesture';
-import Contents from './Contents'
 import HomeScreen from './HomeScreen'
-import Profile from './Profile'
 import DetailScreen from './DetailScreen'
 
-export {Mapview, Animations, Gesture, Contents, HomeScreen, Profile, DetailScreen} ;
+export {HomeScreen, DetailScreen} ;
